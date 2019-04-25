f177aa5fcf0bdc6c36210aa26b5caa88a93326cf
Igor <Mascontris@yahoo.com>
2019-04-24 21:38:03 -0600

    initial commit

---
+++ index.js
@@ -0,0 +1,32 @@
+$(function addItem() {
+    $( "#js-shopping-list-form" ).submit(function( event ) {
+        event.preventDefault();
+        let newItem = $('#shopping-list-entry').val();
+        let newSection =       
+            `<li>
+                <span class="shopping-item">`+newItem+`</span>
+                <div class="shopping-item-controls">
+                    <button class="shopping-item-toggle">
+                        <span class="button-label">check</span>
+                    </button>
+                    <button class="shopping-item-delete">
+                        <span class="button-label">delete</span>
+                    </button>
+                </div>
+            </li>`
+        $('.shopping-list').append(newSection)
+      });
+})
+
+$(function toggleItem() {
+    $('ul').on('click', '.shopping-item-toggle', function() {
+        $(this).closest('li').find('.shopping-item').toggleClass('shopping-item__checked');
+    })
+})  
+
+$(function deleteItem() {
+    $('ul').on('click', '.shopping-item-delete', function() {
+        $(this).closest('li').remove();
+    })
+})
+
